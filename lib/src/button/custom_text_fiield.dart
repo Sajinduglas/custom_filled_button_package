32f31d8e1d9d7238ec11bd33b8d37f56209e03a7
Sajinduglas <sajinduglas500@gmail.com>
2024-06-14 00:39:37 +0530

    edited

--- lib/src/button/custom_text_fiield.dart
+++ lib/src/button/custom_text_fiield.dart
@@ -13,7 +13,7 @@ class CustomFilledButton extends StatelessWidget {
   final Color? textColor;
   final WidgetStateProperty<Color?>? buttonColor;
   final FontWeight? textFontWeight;
-   final String? chekText;
+   final String chekText;
    
      final IconData?  checkIcon;
      
