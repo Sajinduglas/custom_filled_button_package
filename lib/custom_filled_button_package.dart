87b59a27e2f806e4c95c2158ccc3257e47a2341f
Sajinduglas <sajinduglas500@gmail.com>
2024-06-13 13:48:28 +0530

    package completed

--- lib/custom_filled_button_package.dart
+++ lib/custom_filled_button_package.dart
@@ -1,7 +1,2 @@
 library custom_filled_button_package;
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/custom_text.dart';
---
+++ lib/src/custom_text.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class CustomFilledButton extends StatelessWidget {
+  final IconData? icon;
+  final ButtonStyle? buttonStyle;
+  final double? iconSize;
+  final Color? iconColor;
+
+  final VoidCallback? onPressed;
+  final String? buttonText;
+  final double? textFontSize;
+  final Color? textColor;
+  final WidgetStateProperty<Color?>? buttonColor;
+  final FontWeight? textFontWeight;
+
+  const CustomFilledButton({
+    super.key,
+    this.icon,
+    this.buttonStyle,
+    this.iconSize,
+    this.iconColor,
+    this.onPressed,
+    this.buttonText,
+    this.textFontSize,
+    this.textFontWeight,
+    this.textColor,
+    this.buttonColor,
+  }) : assert(icon != null || buttonText != null,
+            'At least one value (icon or buttonText) must be provided.');
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return FilledButton(
+      style: ButtonStyle(
+        padding: WidgetStateProperty.all(
+            EdgeInsets.symmetric(horizontal: 20, vertical: 5)),
+        backgroundColor: buttonColor ??
+            WidgetStateProperty.all<Color>(
+              Colors.blue),
+        shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(
+                6.0), // Rectangular shape with rounded corners
+          ),
+        ),
+      ).merge(buttonStyle),
+      onPressed: onPressed,
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          if (icon != null)
+            FittedBox(
+              child: Icon(icon, size: iconSize, color: iconColor),
+            ),
+          if (icon != null && buttonText != null)
+            SizedBox(width: size.width * .02), // Space between icon and text
+          if (buttonText != null)
+            FittedBox(
+                child: Text(
+              buttonText ?? "",
+              style: TextStyle(
+                fontSize: textFontSize??15,
+                fontWeight: textFontWeight??FontWeight.w500,
+                color: Colors.black ,
+              ),
+            )),
+        ],
+      ),
+    );
+  }
+}
