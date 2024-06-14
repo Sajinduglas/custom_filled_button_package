a2572ee765b59db663568a996ab6a988167bc1c4
Sajinduglas <sajinduglas500@gmail.com>
2024-06-14 17:40:47 +0530

    ui package edited

--- lib/src/button/custom_text_fiield.dart
+++ lib/src/button/custom_text_fiield.dart
@@ -1,103 +1,29 @@
-// import 'package:checkbox_with_text/checkbox_with_text.dart';
-// import 'package:flutter/material.dart';
-// import 'package:login_textfield/login_textfield.dart';
-
-// class CustomFilledButton extends StatelessWidget {
-//   final IconData? icon;
-//   final ButtonStyle? buttonStyle;
-//   final double? iconSize;
-//   final Color? iconColor;
-
-//   final VoidCallback? onPressed;
-//   final String? buttonText;
-//   final double? textFontSize;
-//   final Color? textColor;
-//   final WidgetStateProperty<Color?>? buttonColor;
-//   final FontWeight? textFontWeight;
-
-//   final String? checkButtonText;
-
-//   final String? HeaderText;
-
-//   final TextEditingController? Controller;
-
-//   CustomFilledButton({
-//     super.key,
-//     this.icon,
-//     this.buttonStyle,
-//     this.iconSize,
-//     this.iconColor,
-//     this.onPressed,
-//     this.buttonText,
-//     this.textFontSize,
-//     this.textFontWeight,
-//     this.textColor,
-//     this.buttonColor,
-//     this.HeaderText,
-//     this.Controller,
-//     this.checkButtonText,
-//   }) : assert(icon != null || buttonText != null,
-//             'At least one value (icon or buttonText) must be provided.');
-
-//   @override
-//   Widget build(BuildContext context) {
-//     var size = MediaQuery.of(context).size;
-
-//     return Column(
-//       children: [
-//         FilledButton(
-//           style: ButtonStyle(
-//             padding: WidgetStateProperty.all(
-//                 EdgeInsets.symmetric(horizontal: 20, vertical: 5)),
-//             backgroundColor:
-//                 buttonColor ?? WidgetStateProperty.all<Color>(Colors.blue),
-//             shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-//               RoundedRectangleBorder(
-//                 borderRadius: BorderRadius.circular(
-//                     6.0), // Rectangular shape with rounded corners
-//               ),
-//             ),
-//           ).merge(buttonStyle),
-//           onPressed: onPressed,
-//           child: Row(
-//             mainAxisSize: MainAxisSize.min,
-//             children: [
-//               if (icon != null)
-//                 FittedBox(
-//                   child: Icon(icon, size: iconSize, color: iconColor),
-//                 ),
-//               if (icon != null && buttonText != null)
-//                 SizedBox(
-//                     width: size.width * .02), // Space between icon and text
-//               if (buttonText != null)
-//                 FittedBox(
-//                     child: Text(
-//                   buttonText ?? "",
-//                   style: TextStyle(
-//                     fontSize: textFontSize ?? 15,
-//                     fontWeight: textFontWeight ?? FontWeight.w500,
-//                     color: Colors.black,
-//                   ),
-//                 )),
-//             ],
-//           ),
-//         ),
-//         //  CheckboxWidget(isChecked: true, onChanged: (bool? value) {  }, text:"amos" ,filledButtonText: checkButtonText??"",),
-//         LoginTextfield(
-//           controller: Controller,
-//           headerName: HeaderText,
-//         )
-//       ],
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
 import 'package:nexteon_group_packages/nexteon_group_packages.dart';
 
-
-
 class PackageChecking extends StatelessWidget {
-  const PackageChecking({super.key});
+  final String headerText;
+
+  final String subtext;
+
+  final emailController;
+
+  final String emailText;
+
+  final TextEditingController passwordController;
+
+  final String PasswordText;
+  
+  final void Function() onpressed;
+
+  const PackageChecking(
+      {super.key,
+      required this.headerText,
+      required this.subtext,
+      required this.emailController,
+      required this.emailText,
+      required this.passwordController,
+      required this.PasswordText, required this.onpressed});
 
   @override
   Widget build(BuildContext context) {
@@ -112,30 +38,35 @@ class PackageChecking extends StatelessWidget {
               color: Color(0xffE3F3FF),
             ),
           ),
-        //   SizedBox(
-        //  width:size.width * 0.1,
-        //   ),
+          //   SizedBox(
+          //  width:size.width * 0.1,
+          //   ),
           Padding(
-            padding: const EdgeInsets.only(left: 100,right: 100),
+            padding: const EdgeInsets.only(left: 100, right: 100),
             child: Container(
-              
-            width:500,
+              width: 500,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   SizedBox(
-                   height: size.height * 0.25,
+                    height: size.height * 0.25,
                   ),
                   CustomText(
-                    text: "Welcome back",
+                    text: headerText,
                     style: MyTextSTyles.mainText,
                   ),
                   CustomText(
-                    text: "Please enter Your details",
+                    text: subtext,
                     style: MyTextSTyles.subText,
                   ),
-                  CustomLoginTextField(text: "Email address"),
-                  CustomLoginTextField(text: "password"),
+                  CustomLoginTextField(
+                    text: emailText,
+                    controller: emailController,
+                  ),
+                  CustomLoginTextField(
+                    text: PasswordText,
+                    controller: passwordController,
+                  ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
@@ -145,7 +76,7 @@ class PackageChecking extends StatelessWidget {
                         text: 'Reminder me',
                       ),
                       CustomTextButton(
-                        onPressed: () {},
+                        onPressed: onpressed,
                         buttonText: "Forget password ",
                         // textFontWeight: FontWeight.w500,
                         // textFontSize: 18,
@@ -175,10 +106,8 @@ class PackageChecking extends StatelessWidget {
               ),
             ),
           ),
-        
         ],
       ),
     );
   }
 }
-
